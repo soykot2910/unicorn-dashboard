b43495471a462177766f8fd4dc48607b2f20f830
soykot2910 <md.soykot2910@gmail.com>
2024-10-18 18:57:38 +0600

    initial project setup

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h1 class="text-4xl font-bold">Hello World</h1>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
